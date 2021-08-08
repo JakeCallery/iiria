c9f8d617a6c72cb5fc288ea733f35d67831f7caf
Jake Callery <jake@subvoicestudios.com>
2021-08-08 13:48:06 -0400

    split out weather UI into its own component

---
+++ client/src/components/weatherUI.tsx
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+interface IWeatherProps {
+
+}
+
+interface IWeatherData {
+    temperature: number;
+    preciptationType: number;
+    weatherCode: number;
+    uvIndex: number;
+    uvHealthConcern: number;
+    weatherDesc: string;
+    uvDesc: string;
+    uvHealthDesc: string;
+    precipitationDesc: string;
+}
+
+const defaultWeatherData: IWeatherData = {
+    temperature: -1,
+    preciptationType: -1,
+    weatherCode: -1,
+    uvIndex: -1,
+    uvHealthConcern: -1,
+    weatherDesc: "none",
+    uvDesc: "none",
+    uvHealthDesc: "none",
+    precipitationDesc: "none",
+} 
+
+const WeatherUI = (props: IWeatherProps): JSX.Element => {
+
+    const [weatherData, setWeatherData]:[IWeatherData, (weatherData: IWeatherData)=>void] = useState<IWeatherData>(defaultWeatherData);
+    const [isLoading, setIsLoading]: [boolean, (isLoading: boolean) => void] = useState<boolean>(true);
+    const [error, setError]: [string, (error: string) => void] = useState<string>(''); 
+
+    useEffect(() => {
+        axios
+            .get<IWeatherData>(
+                'http://localhost:9090/weather', 
+                {
+                    headers: {"Content-Type": "application/json"}
+                })
+            .then(response => {
+                setWeatherData(response.data);
+                setIsLoading(false);
+            })
+    }, []);
+
+    return (
+        <div>
+            {isLoading && <div>Loading...</div>}
+            {weatherData.temperature}
+        </div>
+    )
+
+}
+
+export default WeatherUI;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,52 +1,15 @@
-import React, {useState} from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
+import WeatherUI from './components/weatherUI';
 
-interface AppProps {
-    
+interface IAppProps {    
 }
 
-interface IWeatherData {
-    temperature: number;
-    preciptationType: number;
-    weatherCode: number;
-    uvIndex: number;
-    uvHealthConcern: number;
-}
 
-const defaultWeatherData: IWeatherData = {
-    temperature: -1,
-    preciptationType: -1,
-    weatherCode: -1,
-    uvIndex: -1,
-    uvHealthConcern: -1,
-} 
-
-
-
-const App = (props: AppProps): JSX.Element => {
-    const [weatherData, setWeatherData]:[IWeatherData, (weatherData: IWeatherData)=>void] = useState<IWeatherData>(defaultWeatherData);
-    const [isLoading, setIsLoading]: [boolean, (isLoading: boolean) => void] = useState<boolean>(true);
-    const [error, setError]: [string, (error: string) => void] = useState<string>(''); 
-
-    useEffect(() => {
-        axios
-            .get<IWeatherData>(
-                'http://localhost:9090/weather', 
-                {
-                    headers: {"Content-Type": "application/json"}
-                })
-            .then(response => {
-                setWeatherData(response.data);
-                setIsLoading(false);
-            })
-    }, []);
-
+const App = (props: IAppProps): JSX.Element => {
     return (
         <div>
-            {isLoading && <div>Loading...</div>}
-            {weatherData.temperature}
+            <WeatherUI/>
         </div>
     )
 }
